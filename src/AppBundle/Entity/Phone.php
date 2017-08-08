a7c18bd99ea50decbdb46029134d79a61c961e28
Menedyl <nicolas.bostjancic@gmail.com>
2017-08-08 18:23:43 +0200

    Close #2 : Creating resources
    - Creates the "Phone" resource
    - Creates the "Picture" resource
    - Creates the "User" resource
    - Added the relationship between the "Phone" and "Picture" resources

---
+++ src/AppBundle/Entity/Phone.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Phone
+ *
+ * @ORM\Table(name="phone")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PhoneRepository")
+ */
+class Phone
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="price", type="float")
+     */
+    private $price;
+
+    /**
+     * @var Collection
+     *
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Picture", mappedBy="phone", cascade={"all"})
+     */
+    private $pictures;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->pictures = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * Add picture
+     *
+     * @param Picture $picture
+     */
+    public function addPicture(Picture $picture)
+    {
+        $this->pictures->add($picture);
+    }
+
+    /**
+     * Remove picture
+     *
+     * @param Picture $picture
+     */
+    public function removePicture(Picture $picture)
+    {
+        $this->pictures->removeElement($picture);
+    }
+
+    /**
+     * Get pictures
+     *
+     * @return Collection
+     */
+    public function getPictures()
+    {
+        return $this->pictures;
+    }
+}
