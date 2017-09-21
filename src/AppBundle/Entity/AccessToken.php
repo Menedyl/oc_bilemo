86eb3a4356f307f87a9cdb4edd228f0d3561cf03
Menedyl <nicolas.bostjancic@gmail.com>
2017-09-21 14:08:59 +0200

    Fixes 03
    - Various changes

--- src/AppBundle/Entity/AccessToken.php
+++ src/AppBundle/Entity/AccessToken.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\OAuthServerBundle\Entity\AccessToken as BaseAccessToken;
+use FOS\OAuthServerBundle\Model\Client;
 
 /**
  * Class AccessToken
--- src/AppBundle/Service/Representation/Users.php
+++ src/AppBundle/Service/Representation/Users.php
@@ -37,5 +37,4 @@ class Users extends AbstractRepresentation
         return $representationUsers;
     }
 
-
 }
